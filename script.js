f5502c3c62a42c90e5b8b0a749007b8a1a187093
Achinit99 <achinit720@gmail.com>
2022-10-08 09:44:55 +0530

    Coding Challenge #1

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ document.querySelector('.check').addEventListener('click', function () {
     // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#319DA0';
 
@@ -59,3 +60,15 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
 });
+
+//////////////////////////////
+// Coding Challenge #1
+
+/* 
+Implement a game rest functionality, so that the player can make a new guess! Here is how:
+
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and number variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the original background color (#222) and number width (15rem)
+ */
